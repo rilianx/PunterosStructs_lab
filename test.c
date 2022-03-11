401939c3adc1e4c6bb8c66a9f9c265225babec09
Your Name <you@example.com>
2022-03-11 13:06:13 +0000

    fixed format-overflow

--- test.c
+++ test.c
@@ -85,9 +85,9 @@ int test_suma_n(){
     for(i=0; i<10; i++){
         a[i]=rand()%100;
         if(i>=10-m) tot +=a[i];
-        sprintf(msg,"%s %d,",msg,a[i]);
+        sprintf(msg,"%.50s %d,",msg,a[i]);
     }
-    sprintf(msg,"%s]",msg);
+    sprintf(msg,"%.50s]",msg);
     info_msg(msg);
 
     sprintf(msg,"m=%d",m);
