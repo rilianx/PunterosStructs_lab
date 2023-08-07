584dac44456f6b6d8714af9e23778b25874723eb
Replit user <>
2023-08-07 16:07:01 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -8,96 +8,52 @@
 //ejercicios de punteros y arreglos
 
 /*
-Ejercicio 1.
-Programe la función void swap(int *a, int *b), la cual
-intercambia los valores de las variables apuntadas por a y b.
+Ejercicio 1: Encuentra el Elemento Mayor
+Descripción: Escribe una función que tome un arreglo de enteros 
+y su tamaño, y devuelva el valor más grande del arreglo.
 */
-void swap(int *a, int *b) {
+int findMax(int arr[], int size){
 
+  return 0;
 }
 
 /*
-Ejercicio 2.
-Programe la función void arrayMaxMin(int *a, int n, int *max, int *min),
-la cual encuentra el máximo y el mínimo valor del arreglo a y los
-almacena en las variables apuntadas por max y min.
+Ejercicio 2: Invertir un Arreglo
+Descripción: Escribe una función que tome un arreglo y su tamaño, y luego invierta el orden de sus elementos.
 */
-void arrayMaxMin(int *a, int n, int *max, int *min) {
-    
-}
-
-
-/*
-Ejercicio 3.
-Considerando la estructura Persona, programe la función
-crearPersona(...), la cual crea una nueva persona con 
-los datos ingresados como parámetros y retorna un puntero 
-al dato recién creado.
-*/
-typedef struct {
-  char nombre[30];
-  char rut[11];
-  int edad;
-} Persona;
-
-
-Persona* crearPersona(char nombre[], char rut[], int edad) {
-   Persona * p = (Persona *) malloc(sizeof(Persona));
-   //asignar valores de entrada a los campos de p
-
-
-   return p;
+void reverseArray(int arr[], int size){
+  
 }
 
 /*
-Ejercicio 4.
-Considerando la estructura "Vector", escriba 
-la función Vector* crearVector(int n), la cual crea 
-un vector con capacidad `n`, reserva la memoria 
-correspondiente para el arreglo con `n` datos 
-inicializados en 0 y luego retorna el vector creado.
+Ejercicio 3: Filtrar Números Pares
+Descripción: Escribe una función que tome un arreglo de enteros 
+y su tamaño, y luego devuelva un nuevo arreglo que contenga solo 
+los números pares del arreglo original.
 */
-typedef struct {
-   int *datos; // arreglo dinámico
-   int capacidad; // capacidad del arreglo
-} Vector;
+int* filterEvenNumbers(int arr[], int size, int *newSize){
 
-Vector * crearVector(int n) {
-   return NULL;
+  return NULL;
 }
 
 /*
-Ejercicio 5a.
-Programe la función void asignarValor(Vector * v, int i, int valor), 
-la cual asigna el valor a la posición i del vector v.
+Ejercicio 5: Fusión de dos Arreglos Ordenados
+Descripción: Escribe una función que tome dos arreglos 
+ordenados y sus tamaños, y luego fusione estos dos 
+arreglos en un tercer arreglo también ordenado.
 */
-void asignarValor(Vector * v, int i, int valor) {
-
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]){
+  
 }
 
 /*
-Ejercicio 6.
-Programe la función int obtenerValor(Vector * v, int i), 
-la cual retorna el valor en la posición i del vector v.
+Ejercicio 7: Comprobación de Ordenación
+Descripción: Escribe una función que tome un arreglo y su tamaño, 
+y luego devuelva 1 si el arreglo está ordenado en orden ascendente, 
+  0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int obtenerValor(Vector * v, int i) {
-   return 0;
+int checkSorted(int arr[], int size){
+   return -2;
 }
 
-/*
-Ejercicio 7.
-Función que suma los vectores `a` y `b` y 
-actualiza el vector `c` con el resultado de la suma.
-*/
-void sumaV(Vector * a, Vector * b, Vector * c) {
 
-}
-
-/*
-Ejercicio 8.
-Use las operaciones implementadas de vectores para 
-sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
-*/
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-
-}
