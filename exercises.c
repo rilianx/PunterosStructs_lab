e778768d3e604b1dd47d7fa1202015f8e17424c6
Replit user <>
2023-08-07 16:09:38 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -1,59 +1,47 @@
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
 //#include "exercises.h"
 
-//ejercicios de punteros y arreglos
+// ejercicios de punteros y arreglos
 
 /*
 Ejercicio 1: Encuentra el Elemento Mayor
-Descripción: Escribe una función que tome un arreglo de enteros 
+Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size){
-
-  return 0;
-}
+int findMax(int arr[], int size) { return 0; }
 
 /*
 Ejercicio 2: Invertir un Arreglo
-Descripción: Escribe una función que tome un arreglo y su tamaño, y luego invierta el orden de sus elementos.
+Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
+invierta el orden de sus elementos.
 */
-void reverseArray(int arr[], int size){
-  
-}
+void reverseArray(int arr[], int size) {}
 
 /*
 Ejercicio 3: Filtrar Números Pares
-Descripción: Escribe una función que tome un arreglo de enteros 
-y su tamaño, y luego devuelva un nuevo arreglo que contenga solo 
+Descripción: Escribe una función que tome un arreglo de enteros
+y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-int* filterEvenNumbers(int arr[], int size, int *newSize){
-
-  return NULL;
-}
+int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
 
 /*
 Ejercicio 5: Fusión de dos Arreglos Ordenados
-Descripción: Escribe una función que tome dos arreglos 
-ordenados y sus tamaños, y luego fusione estos dos 
+Descripción: Escribe una función que tome dos arreglos
+ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]){
-  
-}
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
+                       int result[]) {}
 
 /*
 Ejercicio 7: Comprobación de Ordenación
-Descripción: Escribe una función que tome un arreglo y su tamaño, 
-y luego devuelva 1 si el arreglo está ordenado en orden ascendente, 
+Descripción: Escribe una función que tome un arreglo y su tamaño,
+y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size){
-   return -2;
-}
-
-
+int checkSorted(int arr[], int size) { return -2; }
